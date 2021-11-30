797dd6ffb0b68186fc64b63b9b8d1828603ebaa4
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2021-11-30 00:41:45 -0500

    POSIBLES ARREGLOS

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,33 @@
+package vehiculos;
+import java.util.ArrayList;
+public class Automovil extends Vehiculo{
+    //ATRIBUTOS
+	protected static int cantidadAutomovil;
+    private int puestos;
+    
+    //CONSTRUCTOR
+    public Automovil(String placa, String nombre, float precio, float peso, Fabricante fabricante, int puestos) {
+        super(placa, 4, 100, nombre, precio, peso,  "FWD", fabricante);
+        this.puestos = puestos;
+        cantidadAutomovil++;
+    }
+    
+    //GET 
+    public int getPuestos() {
+        return puestos;
+    }
+    
+    public static int getCantidadAutomoviles() {
+        return cantidadAutomovil;
+      
+    
+    //SET    
+    public static void setCantidadAutomoviles(int cantidadAutomovil) {
+        Automovil.cantidadAutomovil = cantidadAutomovil;
+    }
+    
+    public void setPuestos(int puestos) {
+        this.puestos = puestos;
+    }
+    
+}
